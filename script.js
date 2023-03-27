e3e08d871c269856e251973034fa48847ee0a4b8
Murillo Cecilio <murilloceciliomoreira@gmail.com>
2023-03-26 21:54:16 -0300

    feito parte do código script

--- script.js
+++ script.js
@@ -3,9 +3,10 @@ function verificar() {
     var ano = data.getFullYear()
     var fAno = document.getElementById('txtano')
     var res = document.getElementById('res')
-    if (fAno.value.length == 0 || fAno.value > ano) {
-        alert('Verifique os dados e tente novamente!')
+    if (fAno.value.length == 0 || Number(fAno.value) > ano) {
+        alert('[ERRO] Verifique os dados e tente novamente!')
     } else {
-        alert('tudo ok')
+        var fsex = document.getElementsByName('radsex')
+        var idade = ano - Number(fAno.value)
     }
 }
